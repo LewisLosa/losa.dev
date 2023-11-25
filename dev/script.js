c1922c11600a8d1099056ab6ee7699e8f12f338e
losa.dev <95869100+LewisLosa@users.noreply.github.com>
2023-11-25 19:02:04 +0300

    Add files via upload

---
+++ dev/script.js
@@ -0,0 +1,19 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '1080',
+        width: '1920',
+        videoId: 'Uw_hZfH5Ukc',
+        playerVars: {
+            'autoplay': 1,
+            'showinfo': 0,
+            'controls': 0,
+            'loop': 1,
+            'iv_load_policy': 3
+        }
+    });
+}
