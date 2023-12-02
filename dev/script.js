a5b1b5e0d4440aa9322d89ba97833ffd356c3546
losa.dev <95869100+LewisLosa@users.noreply.github.com>
2023-12-02 04:47:46 +0300

    Update script.js

--- dev/script.js
+++ dev/script.js
@@ -5,8 +5,8 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('ytplayer', {
-        height: '1440',
-        width: '2560',
+        height: '1080',
+        width: '1920',
         videoId: 'dByHeKrQmq8',
         playerVars: {
             'autoplay': 1,
