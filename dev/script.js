c8caa49dbb1f3517b4bf64968fbfd8d3341461b6
losa.dev <95869100+LewisLosa@users.noreply.github.com>
2024-02-15 13:18:25 +0300

    Update script.js

--- dev/script.js
+++ dev/script.js
@@ -7,7 +7,7 @@ function onYouTubePlayerAPIReady() {
     player = new YT.Player('ytplayer', {
         height: '1080',
         width: '1920',
-        videoId: 'edbY11BX4Xw',
+        videoId: 'edbY11BX4Xw'
         playerVars: {
             'autoplay': 1,
             'showinfo': 0,
